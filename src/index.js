88c256a97fbbb87b420b41eac68d1677988b0d9a
Galen Casstevens <GCasstevens@noregon.com>
2022-01-10 18:16:27 -0500

    set up Emmet and went a little further in the course

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // ReactDOM library allows interaction with the DOM in the browser
+import './index.css'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
