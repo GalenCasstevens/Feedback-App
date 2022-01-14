d557babd43d9d3420a85c8162b43c4ada25dc73c
Galen Casstevens <galencasstevens@gmail.com>
2022-01-14 12:24:46 -0500

    learned a bit about props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom'; // ReactDOM library allows interaction with the DOM in the browser
+import './index.css'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <StrictMode>
+        <App />        
+    </StrictMode>,
+    document.getElementById('root')
+);
