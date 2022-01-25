a36d2f70366d4d3fbc289f9269287312d639c473
Galen Casstevens <galencasstevens@gmail.com>
2022-01-25 08:01:31 -0500

    started adding a form to the app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackItem from './components/FeedbackItem';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -16,6 +17,7 @@ function App() {
 		<>
 			<Header />
 			<div className="container">
+				<FeedbackForm />
 				<FeedbackStats feedback={feedback} />
 				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
