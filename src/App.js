61216de53d457ffbcfeb73a21246bb69e3a96024
Galen Casstevens <galencasstevens@gmail.com>
2022-01-31 21:32:08 -0500

    finished adding ability to add feedback

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
-import FeedbackItem from './components/FeedbackItem';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
@@ -9,7 +9,10 @@ import FeedbackForm from './components/FeedbackForm';
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
 
-	const addFeedback = (newFeedback) => {};
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4();
+		setFeedback([newFeedback, ...feedback]);
+	};
 
 	const deleteFeedback = (id) => {
 		if (window.confirm('Are you sure you want to delete?'))
