b1832683d4c3d22e53d3d1c3ba7700a8e744aba8
Galen Casstevens <GCasstevens@noregon.com>
2022-02-08 12:48:10 -0500

    refactored the app by using context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { createContext, useState } from 'react';
 import FeedbackData from '../data/FeedbackData';
 
@@ -6,10 +7,22 @@ const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
 	const [feedback, setFeedback] = useState(FeedbackData);
 
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4();
+		setFeedback([newFeedback, ...feedback]);
+	};
+
+	const deleteFeedback = (id) => {
+		if (window.confirm('Are you sure you want to delete?'))
+			setFeedback(feedback.filter((item) => item.id !== id));
+	};
+
 	return (
 		<FeedbackContext.Provider
 			value={{
 				feedback,
+				addFeedback,
+				deleteFeedback,
 			}}
 		>
 			{children}
