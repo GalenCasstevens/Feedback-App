adb2a83eb6f54cf451f5f02c2b8963f9633c8194
Galen Casstevens <galencasstevens@gmail.com>
2022-02-07 22:16:41 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { v4 as uuidv4 } from 'uuid';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
@@ -7,6 +6,7 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -25,11 +25,26 @@ function App() {
 		<Router>
 			<Header />
 			<div className="container">
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}
+					></Route>
 
-				<Route path="/about" component={AboutPage} />
+					<Route path="/about" element={<AboutPage />} />
+				</Routes>
+
+				<AboutIconLink />
 			</div>
 		</Router>
 	);
