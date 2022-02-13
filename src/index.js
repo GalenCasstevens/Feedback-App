c804546c11577f8eb78a1b2fb47e7d577dc7099e
Galen Casstevens <galencasstevens@gmail.com>
2022-02-12 19:57:49 -0500

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom' // ReactDOM library allows interaction with the DOM in the browser
+import ReactDOM from 'react-dom'; // ReactDOM library allows interaction with the DOM in the browser
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
